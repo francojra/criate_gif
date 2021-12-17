f8dbd64fea87d2a4687b5599c0cda10b6c82ca2e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-12-16 23:43:56 -0300

    Add files via upload

--- criar_gif_r.R
+++ criar_gif_r.R
@@ -1,2 +1,14 @@
 # Criando gifs no R
 
+library("magick")
+
+png_1 <- image_read("arv_natal.jpg")
+png_2 <- image_read("arv_natal_2.jpg")
+
+gif <- c(png_1, png_2)
+
+animacao <- image_animate(image_scale(gif, "600"),
+                          fps = 0.5,
+                          dispose = "previous")
+
+image_write(animacao, "anim.gif")
